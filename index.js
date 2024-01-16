927e4a219eb00c859ea23e8ef5411526bd00ba99
Karthik Hallad <karthikhallad@gmail.com>
2024-01-16 21:49:34 +0530

    addFolders

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const cors = require('cors')
 const app = express()
 const PORT = process.env.PORT || 3001
 
-
+//serve react files..index.html
+app.use(express.static('dist'))
 
 var bodyParser = require('body-parser')
 // parse application/x-www-form-urlencoded
